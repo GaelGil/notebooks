12935d33b2fe97eb188ec6512a597a2db373e587
Gael Gil <gaelgilbello27@gmail.com>
2020-07-20 21:46:56 -0700

    working on image filtering stuff

---
+++ build_opencv/apply_filter.py
@@ -0,0 +1,10 @@
+
+
+
+def apply_filter(img, filter):
+    """
+    This function takes in a image as its argument (numpy array)
+    and a filter (some array with n dimmensions). We will multiply
+    the filter by the array to get a filtered image.
+    """
+    return filtered_image
