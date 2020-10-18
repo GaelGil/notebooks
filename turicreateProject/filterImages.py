413e749d94708788efd66163f2c54c6757ea780c
Gael Gil <gaelgilbello27@gmail.com>
2020-10-18 00:13:50 -0700

    added some code to filter images

--- turicreateProject/filterImages.py
+++ turicreateProject/filterImages.py
@@ -7,6 +7,9 @@ edgeFilter = np.array([[-1, -1, -1],
                        [-1, 8, -1],
                        [-1, -1, -1]])
 
+customFilter = np.array([[1, 0, 0],
+                          [0, 1, 0],
+                          [0, 0, 1]])
 
 
 def loadImagesFromFolder(folder):
@@ -25,15 +28,44 @@ def loadImagesFromFolder(folder):
                     pass
                 else:
                     # 
-                    directory = "./newDataset/" + subFolder
+                    directory = "./expandedDataset/" + subFolder
                     # set image path
                     image_path = folder + "/" + subFolder + "/" + filename
                     # use opencv to read the image
                     img = cv2.imread(image_path) 
+
+                    print(image_path)
                     # apply filter
-                    filteredImg = filter(img, edgeFilter)
+                    # filteredImg = filter(img, edgeFilter)
+
+                    # black and white image
+                    blackAndGrey = cv2.cvtColor(img.copy(), cv2.COLOR_BGR2GRAY)
+                    # make a slighlty blurred image
+                    blurred = cv2.GaussianBlur(img.copy(), (21,21), 0)
+                    # make a saturated image
+                    saturated = cv2.filter2D(img.copy(), -100, customFilter)
+                    # flip image horizontally
+                    horizontal = cv2.flip(img.copy(), 1)
+                    # flip image vertically
+                    veritcally = cv2.flip(img.copy(), 0)
+
                     # save it to new folder
-                    cv2.imwrite(os.path.join(directory , filename), filteredImg)
+                    cv2.imwrite(os.path.join(directory , filename), img.copy())
+                    cv2.waitKey(0)
+
+                    cv2.imwrite(os.path.join(directory , filename), blackAndGrey)
+                    cv2.waitKey(0)
+                   
+                    cv2.imwrite(os.path.join(directory , filename), blurred)
+                    cv2.waitKey(0)
+
+                    cv2.imwrite(os.path.join(directory , filename), saturated)
+                    cv2.waitKey(0)
+
+                    cv2.imwrite(os.path.join(directory , filename), horizontal)
+                    cv2.waitKey(0)
+
+                    cv2.imwrite(os.path.join(directory , filename), veritcally)
                     cv2.waitKey(0)
 
 
