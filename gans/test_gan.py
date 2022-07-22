6d341ef28e3bba2d567fcba9a41aa843adcc663d
gaelgil <gaelgilbello27@gmail.com>
2022-07-21 18:08:03 -0700

    working on gan

---
+++ gans/test_gan.py
@@ -0,0 +1,8 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+from torchvision import datasets, transforms
+from torchvision.datasets import ImageFolder
