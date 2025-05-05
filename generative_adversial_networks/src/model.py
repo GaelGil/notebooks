472d5246e9f16343af9e5ad7307bc29b323f563f
Gael <gaelgilbello27@gmail.com>
2025-05-05 15:52:16 -0700

    implemented generator

--- generative_adversial_networks/src/model.py
+++ generative_adversial_networks/src/model.py
@@ -44,7 +44,43 @@ class ConvBlock(nn.Module):
 
 
 class Generator(nn.Module):
-    pass
+    def __init__(self, z_dim,  in_channels, img_channels=3):
+        super().__init__()
+        self,initial = nn.Sequential(
+            PixelNorm(), 
+            nn.ConvTranspose2d(z_dim, in_channels, 4, 1, 0), # 1 x 1 to 4 x 4
+            nn.LeakyReLU(0.2),
+            WSConv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1),
+            nn.LeakyReLU(0.2),
+            PixelNorm(),
+        )
+
+        self.initial_rgb = WSConv2d(in_channels, img_channels, kernel_size=1, stride=1)
+        self.prog_blocks = nn.ModuleList()
+        self.rgb_layers = nn.ModuleList(self.initial_rgb)
+        for i in range(len(factors)-1):
+            conv_in_c = int(in_channels * factors[i])
+            conv_out_c = int(in_channels* factors[i+1])
+            self.prog_blocks.append(ConvBlock(conv_in_c, conv_out_c))
+            self.rgb_layers.append(WSConv2d(conv_out_c, img_channels, kernel_size=1, stride=1))
+
+    def fade_in(self, alpha, upscaled, generated):
+        return torch.tanh(alpha * generated + (1-alpha) * upscaled)
+
+    def forward(self, x, alpha, steps):
+        out = self.initial(x)
+        if steps == 0:
+            return self.initial_rgb(out)
+        
+        for step in range(steps):
+            upscaled = F.interpolate(out, scale_factor=2, mode='nearest')
+            out = self.prog_blocks[step](upscaled)
+
+        final_upscaled = self.rgb_layers[steps-1](upscaled)
+        final_out = self.rgb_layers[steps](out)
+
+
+        return self.fade_in(alpha, final_upscaled, final_out)
 
 class Discriminator(nn.Module):
     pass
