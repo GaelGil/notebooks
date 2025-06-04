2295a23f6295e32091898328149d2de029c263e5
GaelGil <31393159+GaelGil@users.noreply.github.com>
2025-06-04 11:13:40 -0700

    updating structure

---
+++ generative_adversial_networks/src/gan/config.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+LEARNING_RATE = 2e-4  
+BATCH_SIZE = 128
+IMAGE_SIZE = 64
+CHANNELS_IMG = 1
+NOISE_DIM = 100
+NUM_EPOCHS = 5
+FEATURES_DISC = 64
+FEATURES_GEN = 64
