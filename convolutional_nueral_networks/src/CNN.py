7c3e26d2b06d89f818098390aee8ca9a1dd604fc
GaelGil <31393159+GaelGil@users.noreply.github.com>
2025-06-05 13:22:50 -0700

    updating model

--- convolutional_nueral_networks/src/CNN.py
+++ convolutional_nueral_networks/src/CNN.py
@@ -16,22 +16,33 @@ class CNN(nn.Module):
             Function to feed an input forward through the model
 
     """
-    def __init__(self, in_channels, classes, kernel_size):
+    def __init__(self, in_channels, num_classes, kernel_size) -> None:
         """Function to initialize the cnn model
+
+        Args:
+            in_channels: The number of input chanels from our inputs
+            classes: the number of classes/outputs
+            kernel_size: the size of the kernels/matrices
+
+        Returns:
+            None
         """
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=6, kernel_size=kernel_size)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=1)
         self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=kernel_size)
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=kernel_size)
-        # got the input for fc1 by passing a sample image
-        self.fc1 = nn.Linear(32*28*28, 120)
-        self.fc2 = nn.Linear(120, classes)
+        self.fc1 = nn.Linear(32*28*28, 500)
+        self.fc2 = nn.Linear(500, 100)
+        self.fc3 = nn.Linear(100, num_classes)
 
     def forward(self, x):
         """Function to feed an input forward through the model
         Args:
             x: The input
+
+        None:
+            prediction
         """
         # convolution layer -> relu -> maxpool 2x2 (stride 1)
         x = self.pool(F.relu(self.conv1(x)))
@@ -43,6 +54,8 @@ class CNN(nn.Module):
         x = torch.flatten(x, 1)
         # first fully connected layer
         x = self.fc1(x)
-        # second fully 
+        # first fully connected layer
         x = self.fc2(x)
+        # last layer 
+        x = self.fc3(x)
         return  x
