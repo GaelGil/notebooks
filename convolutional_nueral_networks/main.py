a7fc7478f2ce686104367721de6b1fa1ed53f968
GaelGil <31393159+GaelGil@users.noreply.github.com>
2025-06-17 14:58:33 -0700

    updated to train and evaluate

--- convolutional_nueral_networks/main.py
+++ convolutional_nueral_networks/main.py
@@ -4,8 +4,7 @@ import torch.optim as optim
 from torchvision.datasets import ImageFolder
 from src import confing
 from src.CNN import CNN
-from utils import train, evaluate, check_accuracy
-from src.accuracy import check_accuracy
+from utils import train, evaluate
 
 if __name__ == "__main__":
     # load in the dataset
@@ -22,34 +21,43 @@ if __name__ == "__main__":
     valid_dataset_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=confing.BATCH_SIZE, shuffle=True)
     test_dataset_loader = torch.utils.data.DataLoader(test_dataset, batch_size=confing.BATCH_SIZE, shuffle=True)
 
-
-    epochs = [25, 50 ,75, 100, 150]
-    lr_rates = [0.001, 0.01]
-    dropout_rates = [0.25, 0.5]
-
-
-
-    for epoch in epochs:
-        for lr in lr_rates:
-            for dropout in dropout_rates:
+    # train and evaluate the model and select the best params
+    best_accuracy = 0
+    best_params = {}
+    for epoch in confing.EPOCHS:
+        for lr in confing.LR_RATES:
+            for dropout in confing.DROPOUT_RATES:
                 model = CNN(dropout_rate=dropout).to(confing.DEVICE)
                 optimizer = optim.Adam(model.parameters(), lr=lr)
                 criterion = nn.CrossEntropyLoss()
-
                 train(model=model, train_loader=train_dataset_loader, optimizer=optimizer, epochs=epoch, device=confing.DEVICE)
+                eval_accuracy = evaluate(model=model, device=confing.DEVICE, val_loader=valid_dataset_loader)
+                print(f'Epochs: {epoch}, LR {lr}, Dropout: {dropout}, Val Accuracy: {eval_accuracy:.4f}')
+                if eval_accuracy > best_accuracy:
+                    best_acc = eval_accuracy
+                    best_params = {'epoch': epoch, 'lr': lr, 'dropout': dropout}
+
 
     # initialize the model
-    model = CNN(in_channels=confing.IN_CHANNELS, num_classes=confing.NUM_CLASSES, kernel_size=confing.KERNEL_SIZE).to(confing.DEVICE)
+    model = CNN(in_channels=confing.IN_CHANNELS,
+                num_classes=confing.NUM_CLASSES,
+                kernel_size=confing.KERNEL_SIZE,
+                dropout_rate=best_params['dropout']).to(confing.DEVICE)
+    
     # set the optimizer
-    optimizer = optim.Adam(model.parameters(), lr=confing.LEARNING_RATE)
+    optimizer = optim.Adam(model.parameters(), lr=confing.best_params['lr'])
 
     # train the model
-    train(model=model, train_loader=train_dataset_loader, optimizer=optimizer, epochs=confing.EPOCHS, device=confing.DEVICE)
+    train(model=model,
+          train_loader=train_dataset_loader,
+          optimizer=optimizer,
+          epochs=best_params['epoch'],
+          device=confing.DEVICE)
+
+
+    # evalutate model 
+    evaluate(loader=test_dataset_loader, model=model, device=confing.DEVICE)
 
     # save model
     torch.save(model.state_dict(), confing.MODEL_PATH)
 
-
-
-    # get accuracy
-    # check_accuracy(test)
