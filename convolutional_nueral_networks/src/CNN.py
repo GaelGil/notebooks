d34d822fb34e1da76afc8077aef57cc04f7a6c03
gaelgil <gaelgilbello27@gmail.com>
2025-07-02 19:19:35 -0700

    updated model to have differnert pool layer names

--- convolutional_nueral_networks/src/CNN.py
+++ convolutional_nueral_networks/src/CNN.py
@@ -45,9 +45,11 @@ class CNN(nn.Module):
         """
         super().__init__()
         self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=8, kernel_size=kernel_size, padding=padding, stride=stride)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv2 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=kernel_size, padding=padding, stride=stride)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.conv3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=kernel_size, padding=padding, stride=stride)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
         self.dropout = nn.Dropout(dropout_rate)
         self.fc1 = nn.Linear(28*28*32, 500)
         self.fc2 = nn.Linear(500, 100)
@@ -62,11 +64,11 @@ class CNN(nn.Module):
             prediction
         """
         # convolution layer -> relu -> maxpool 2x2 (stride 1)
-        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool1(F.relu(self.conv1(x)))
         # convolution layer -> relu -> maxpool 2x2 (stride 1)
-        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
         # convolution layer -> relu -> maxpool 2x2 (stride 1)
-        x = self.pool(F.relu(self.conv3(x)))
+        x = self.pool3(F.relu(self.conv3(x)))
         # flatten our output from the convolutional layers
         x = torch.flatten(x, 1)
         # first fully connected layer
