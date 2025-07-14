d885618e8eb67aafdb13861a57db11875d50de18
Gael <gaelgilbello27@gmail.com>
2025-07-14 10:54:43 -0700

    adding more utils

--- convolutional_nueral_networks/src/config.py
+++ convolutional_nueral_networks/src/config.py
@@ -1,19 +1,21 @@
 import torch
 from torchvision import transforms
 
-DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-IMG_TRANSFORMATIONS = transforms.Compose([
-    transforms.Resize((255, 255)),
-    transforms.ToTensor(),
-   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-DATA_PATH = './data/PetImages'
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+IMG_TRANSFORMATIONS = transforms.Compose(
+    [
+        transforms.Resize((255, 255)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
+DATA_PATH = "./data/PetImages"
 IN_CHANNELS = 3
 NUM_CLASSES = 1
 KERNEL_SIZE = 5
-EPOCHS = [10, 15]
-LR_RATES= [0.00001, 0.0001, 0.001]
-DROPOUT_RATES = [0.25, 0.5]
+EPOCHS = [20, 25]
+LR_RATES = [0.00001, 0.0001, 0.001]
+DROPOUT_RATES = [0, 0.25, 0.5]
 BATCH_SIZE = 64
-NUM_WORKERS=8
-MODEL_PATH = './models/model_state_dict.pth'
+NUM_WORKERS = 8
+MODEL_PATH = "./models/model_state_dict.pth"
