49251dc0698cd400824a47d31046c9c8748c731d
gaelgil <gaelgilbello27@gmail.com>
2025-07-15 17:59:34 -0700

    trained models

--- convolutional_nueral_networks/src/config.py
+++ convolutional_nueral_networks/src/config.py
@@ -5,6 +5,9 @@ DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 IMG_TRANSFORMATIONS = transforms.Compose(
     [
         transforms.Resize((255, 255)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ]
@@ -13,8 +16,8 @@ DATA_PATH = "./data/PetImages"
 IN_CHANNELS = 3
 NUM_CLASSES = 1
 KERNEL_SIZE = 5
-EPOCHS = [30]
-LR_RATES = [0.00001, 0.0001, 0.001]
+EPOCHS = [20, 30]
+LR_RATES = [0.00001, 0.0001, 0.01]
 DROPOUT_RATES = [0, 0.25, 0.5]
 BATCH_SIZE = 64
 NUM_WORKERS = 8
