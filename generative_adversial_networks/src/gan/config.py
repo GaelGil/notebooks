2f416457a09a2eaf8985316c0fd5327977c15e45
Gael <gaelgilbello27@gmail.com>
2025-07-17 12:12:32 -0700

    updating types

--- generative_adversial_networks/src/gan/config.py
+++ generative_adversial_networks/src/gan/config.py
@@ -1,7 +1,18 @@
 import torch
 import torch.nn as nn
+from torchvision import transforms
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+IMG_TRANSFORMATIONS = transforms.Compose(
+    [
+        transforms.Resize((255, 255)),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ]
+)
 LEARNING_RATE = 2e-4
 BATCH_SIZE = 128
 IMAGE_SIZE = 64
@@ -14,4 +25,4 @@ KERNEL_SIZE = 4
 STRIDE = 2
 PADDING = 1
 NUM_WORKERS = 8
-MEMORY_PIN = True
+PIN_MEMORY = True
