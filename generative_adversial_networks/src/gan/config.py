028f7675273ea3bb6733d76e7014fa0712e2f5f8
Gael <gaelgilbello27@gmail.com>
2025-07-17 11:39:38 -0700

    updated types

--- generative_adversial_networks/src/gan/config.py
+++ generative_adversial_networks/src/gan/config.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-LEARNING_RATE = 2e-4  
+LEARNING_RATE = 2e-4
 BATCH_SIZE = 128
 IMAGE_SIZE = 64
 CHANNELS_IMG = 1
@@ -13,3 +13,5 @@ FEATURES_GEN = 64
 KERNEL_SIZE = 4
 STRIDE = 2
 PADDING = 1
+NUM_WORKERS = 8
+MEMORY_PIN = True
