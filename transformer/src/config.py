3c6c0320c987b83cf5738b59fbed52ec68d56132
Gael <gaelgilbello27@gmail.com>
2025-09-03 19:26:34 -0700

    updating stuff

---
+++ transformer/src/config.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+import torch
+from torchvision import transforms
+
+
+class Config(BaseModel):
+    DEVICE: str
+    DATA_PATH: str
+    BATCH_SIZE: int
+    NUM_WORKERS: int
+    MODEL_PATH: str
+
+
+config = Config(DEVICE=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"))
