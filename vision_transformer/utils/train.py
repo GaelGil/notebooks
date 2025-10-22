cb9e97894682da5352155e8fd8b256a02ab66428
Gael Gil <gaelgilbello27@gmail.com>
2025-10-21 21:59:16 -0700

    added train file not yet implemented

---
+++ vision_transformer/utils/train.py
@@ -0,0 +1,19 @@
+from utils.config import config
+from utils.utils import build_vision_transformer
+from vision_transformer.model import VisionTransformer
+
+
+def train():
+    model: VisionTransformer = build_vision_transformer(
+        src_vocab_size=config.SRC_VOCAB_SIZE,
+        target_vocab_size=config.TARGET_VOCAB_SIZE,
+        src_seq_len=config.SRC_SEQ_LEN,
+        target_seq_len=config.TARGET_SEQ_LEN,
+        patch_size=config.PATCH_SIZE,
+        d_model=config.D_MODEL,
+        N=config.N,
+        h=config.H,
+        d_ff=config.D_FF,
+        dropout=config.DROPOUT,
+    )
+    return model
