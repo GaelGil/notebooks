8cd931de3c306d65c4e229b42a4a705b5ad2f3ca
Gael Gil <gaelgilbello27@gmail.com>
2025-10-22 23:29:04 -0700

    more updates

--- vision_transformer/utils/config.py
+++ vision_transformer/utils/config.py
@@ -6,18 +6,15 @@ class Config(BaseModel):
     DATA_PATH: str
     BATCH_SIZE: int
     NUM_WORKERS: int
+    NUM_PATCHES: int
     MODEL_PATH: str
     NUM_CLASSES: int
-    SRC_VOCAB_SIZE: int
-    TARGET_VOCAB_SIZE: int
-    SRC_SEQ_LEN: int
-    TARGET_SEQ_LEN: int
     D_MODEL: int
     N: int
     H: int
     D_FF: int
     DROPOUT: float
-    PATCHES: int
+    IMG_SIZE: int
     PATCH_SIZE: int
 
 
@@ -28,15 +25,12 @@ config = Config(
     NUM_WORKERS=8,
     MODEL_PATH="model.pth",
     NUM_CLASSES=100,
-    SRC_VOCAB_SIZE=100,
-    TARGET_VOCAB_SIZE=100,
-    SRC_SEQ_LEN=100,
-    TARGET_SEQ_LEN=100,
     D_MODEL=512,
     N=6,
     H=8,
     D_FF=2048,
     DROPOUT=0.1,
-    PATCHES=16,
     PATCH_SIZE=16,
+    IMG_SIZE=32,
+    NUM_PATCHES=(32 // 16) ** 2,
 )
