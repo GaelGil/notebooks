f79ff34b3868f2a2b4e77d51394e292022f4859e
Gael Gil <gaelgilbello27@gmail.com>
2025-10-24 14:11:26 -0700

    updating more stuff

--- vision_transformer/utils/train_eval.py
+++ vision_transformer/utils/train_eval.py
@@ -1,6 +1,7 @@
 import jax.numpy as jnp
 import optax
 from flax import nnx
+from flax.training import train_state
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
@@ -9,12 +10,12 @@ from vision_transformer.model import VisionTransformer
 
 def train(
     model: VisionTransformer,
+    state: train_state.TrainState,
     train_loader: DataLoader,
     val_loader: DataLoader,
     optimizer: nnx.Optimizer,
     num_epochs: int,
 ):
-    model.train()
     # loop over the dataset for num_epochs
     for epoch in range(num_epochs):
         # create a tqdm progress bar
@@ -24,17 +25,26 @@ def train(
         # iterate through each batch in the dataset
         for batch in train_loader:
             # train on batch
-            train_step(model=model, optimizer=optimizer, batch=batch)
+
+            state, loss = (
+                train_step(model=model, state=state, optimizer=optimizer, batch=batch),
+            )
 
         # after each epoch, evaluate on train and val set
         progress_bar.set_postfix(
             train_accuracy=eval(model=model, val_loader=train_loader),
             eval_accuracy=eval(model=model, val_loader=val_loader),
         )
+    return state
 
 
 @nnx.jit
-def train_step(model: VisionTransformer, optimizer: nnx.Optimizer, batch):
+def train_step(
+    model: VisionTransformer,
+    state: train_state.TrainState,
+    optimizer: nnx.Optimizer,
+    batch,
+):
     """
     Training step as implemented in
     "https://cloud.google.com/blog/products/ai-machine-learning/guide-to-jax-for-pytorch-developers"
@@ -59,7 +69,8 @@ def train_step(model: VisionTransformer, optimizer: nnx.Optimizer, batch):
     grad_fn = nnx.value_and_grad(loss_fn)
     loss, grads = grad_fn(model)
     # update the parameters
-    optimizer.update(grads)
+    state = state.apply_gradients(grads=grads)
+    return state, loss
 
 
 def eval(model: VisionTransformer, val_loader: DataLoader):
