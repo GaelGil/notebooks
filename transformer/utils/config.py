39927d936ad9bc06a0cfba91b4893e5b1ac971c4
Gael Gil <gaelgilbello27@gmail.com>
2025-10-23 21:03:21 -0700

    updated config and model init

--- transformer/utils/config.py
+++ transformer/utils/config.py
@@ -2,35 +2,47 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    DEVICE: str
-    DATA_PATH: str
     BATCH_SIZE: int
-    NUM_WORKERS: int
-    MODEL_PATH: str
+    NUM_EPOCHS: int
+    LR: float
+    SEQ_LEN: int
+    D_MODEL: int
     SRC_VOCAB_SIZE: int
     TARGET_VOCAB_SIZE: int
-    SRC_SEQ_LEN: int
-    TARGET_SEQ_LEN: int
-    D_MODEL: int
-    N: int
-    H: int
     D_FF: int
+    H: int
+    N: int
+    LANG_SRC: str
+    LANG_TARGET: str
+    MODEL_FOLDER: str
+    MODEL_BASENAME: str
+    PRELOAD: None
+    TOKENIZER_FILE: str
+    EXPERIMENT_NAME: str
     DROPOUT: float
+    DEVICE: str
+    DATA_PATH: str
 
 
 config = Config(
-    DEVICE="DEVICE",
-    DATA_PATH="data",
     BATCH_SIZE=32,
-    NUM_WORKERS=8,
-    MODEL_PATH="model.pth",
-    SRC_VOCAB_SIZE=100,
-    TARGET_VOCAB_SIZE=100,
-    SRC_SEQ_LEN=100,
-    TARGET_SEQ_LEN=100,
+    NUM_EPOCHS=10,
+    LR=10e-4,
+    SEQ_LEN=350,
     D_MODEL=512,
-    N=6,
-    H=8,
+    SRC_VOCAB_SIZE=8000,
+    TARGET_VOCAB_SIZE=8000,
     D_FF=2048,
+    H=8,
+    N=6,
+    LANG_SRC="span",
+    LANG_TARGET="nau",
+    MODEL_FOLDER="./models",
+    MODEL_BASENAME="tmodel_",
+    PRELOAD=None,
+    TOKENIZER_FILE="tokenizer.json",
+    EXPERIMENT_NAME="runs/model",
     DROPOUT=0.1,
+    DEVICE="CUDA",
+    DATA_PATH="./data",
 )
--- transformer/utils/train.py
+++ transformer/utils/train.py
@@ -3,15 +3,14 @@ from utils.config import config
 
 
 def train():
-    print(config)
     model: Transformer = Transformer(
-        src_vocab_size=config.SRC_VOCAB_SIZE,
-        target_vocab_size=config.TARGET_VOCAB_SIZE,
-        seq_len=config.SRC_SEQ_LEN,
         d_model=config.D_MODEL,
         N=config.N,
         n_heads=config.H,
         d_ff=config.D_FF,
         dropout=config.DROPOUT,
+        seq_len=config.SEQ_LEN,
+        src_vocab_size=config.SRC_VOCAB_SIZE,
+        target_vocab_size=config.TARGET_VOCAB_SIZE,
     )
     return model
