27eb66332438e821b84aaba89e3be205ac69d881
Gael Gil <gaelgilbello27@gmail.com>
2025-10-31 11:28:45 -0700

    working on tokenizer

--- transformer/utils/TokenizeDataset.py
+++ transformer/utils/TokenizeDataset.py
@@ -13,6 +13,17 @@ class TokenizeDataset:
         language,
         tokenizer_path,
     ):
+        """
+        Tokenize the dataset
+
+        Args:
+            dataset: dataset to tokenize
+            language: language to tokenize
+            tokenizer_path: path to save the tokenizer
+
+        Returns:
+            None
+        """
         self.language = language
         self.tokenizer_path = Path(tokenizer_path.format(language))
         tokenizer = Tokenizer(WordLevel(unk_token="[UNK]"))
@@ -25,20 +36,41 @@ class TokenizeDataset:
         tokenizer.save(str(self.tokenizer_path))
         self.tokenizer = tokenizer
 
-    def get_tokenizer(self):
+    def get_tokenizer(self) -> Tokenizer:
+        """
+        Get the tokenizer
+
+        Args:
+            None
+
+        Returns:
+            Tokenizer
+        """
         return self.tokenizer
 
-    def encode(self, example, max_len=128):
-        src = (
+    def encode(self, example, max_len=128) -> dict:
+        """
+        Encode the dataset
+
+
+        Args:
+            example: example from the dataset
+            max_len: maximum length of the sequence
+
+        Returns:
+            Dict containing the encoded sequence
+
+        """
+        seq_ids = (
             [self.tokenizer.token_to_id("[SOS]")]
-            + self.tokenizer.encode(example["en"]).ids
+            + self.tokenizer.encode(example[self.language]).ids
             + [self.tokenizer.token_to_id("[EOS]")]
         )
 
-        src = src[:max_len] + [self.tokenizer.token_to_id("[PAD]")] * (
-            max_len - len(src)
+        seq_ids = seq_ids[:max_len] + [self.tokenizer.token_to_id("[PAD]")] * (
+            max_len - len(seq_ids)
         )
 
         return {
-            "ids": src,
+            "ids": seq_ids,
         }
