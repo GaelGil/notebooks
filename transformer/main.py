601a05504e918e4c62ec9b80c1c1c5afa614d7f0
Gael Gil <gaelgilbello27@gmail.com>
2025-10-31 22:47:00 -0700

    updating the dataset class

--- transformer/main.py
+++ transformer/main.py
@@ -39,20 +39,33 @@ def main():
         tokenizer_path=config.TOKENIZER_FILE,
     )
 
-    # get the token ids
+    # get the token ids for the dataset
     logger.info("Getting the token ids ...")
     src_ids = src_tokenizer.get_token_ids()
     target_ids = target_tokenizer.get_token_ids()
 
+    # set the token ids for the dataset
     lang_dataset.set_src_target_ids(src_ids=src_ids, target_ids=target_ids)
 
-    train_loader, val_loader, test_loader = lang_dataset.split_data(
+    # split the src and target ids into train, val and test sets
+    lang_dataset.split(
         train_split=config.TRAIN_SPLIT,
         val_split=config.VAL_SPLIT,
         batch_size=config.BATCH_SIZE,
     )
 
-    train_laoder, val_loader, test_loader = lang_dataset.get_loaders()
+    # get the loaders
+    (
+        src_train_loader,
+        src_val_loader,
+        src_test_loader,
+        target_train_loader,
+        target_val_loader,
+        target_test_loader,
+    ) = lang_dataset.get_loaders()
+
+    for batch in src_test_loader:
+        print(batch)
 
     # # logger.info("Splitting the dataset into train, val and test sets")
     # # # split the dataset
