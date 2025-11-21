7b796590cc50b91ee6518805edf7303ff613b3f0
Gael Gil <gaelgilbello27@gmail.com>
2025-11-20 23:55:35 -0800

    adding handler

--- transformer/main.py
+++ transformer/main.py
@@ -2,9 +2,7 @@ import jax
 from absl import logging
 
 from utils.config import config
-from utils.LangDataset import LangDataset
-from utils.Model import Model
-from utils.TokenizeDataset import TokenizeDataset
+from utils.init_train_state import init_train_state
 
 logging.set_verbosity(logging.INFO)
 
@@ -14,65 +12,61 @@ def main():
     device = jax.devices("gpu")[0]
     logging.info(f"Using device: {device}")
 
-    model = Model(
-        model_name=config.MODEL_NAME,
-        tokenizer=config.TOKENIZER,
-        model=config.MODEL,
-    )
-
-    model = model.get_model()
+    # initialize the train state
+    logging.info("Initializing the train state ...")
+    train_state = init_train_state(config)
 
     # load the dataset
-    logging.info(f"Loading Dataset from: {config.DATA_PATH}")
-    lang_dataset = LangDataset(
-        dataset_name=config.DATA_PATH,
-        src_lang=config.LANG_SRC,
-        target_lang=config.LANG_TARGET,
-    )
-    logging.info(f"Length of dataset: {lang_dataset.__len__()}")
-    lang_dataset.handle_null()
-    logging.info(f"Length of dataset after handling null: {lang_dataset.__len__()}")
+    # logging.info(f"Loading Dataset from: {config.DATA_PATH}")
+    # lang_dataset = LangDataset(
+    #     dataset_name=config.DATA_PATH,
+    #     src_lang=config.LANG_SRC,
+    #     target_lang=config.LANG_TARGET,
+    # )
+    # logging.info(f"Length of dataset: {lang_dataset.__len__()}")
+    # lang_dataset.handle_null()
+    # logging.info(f"Length of dataset after handling null: {lang_dataset.__len__()}")
 
-    # tokenize the dataset in both languages using the entire dataset
-    logging.info("Tokenizing the dataset ...")
-    src_tokenizer = TokenizeDataset(
-        dataset=lang_dataset.dataset["train"],
-        language=config.LANG_SRC,
-        tokenizer_path=config.TOKENIZER_FILE,
-    )
-    target_tokenizer = TokenizeDataset(
-        dataset=lang_dataset.dataset["train"],
-        language=config.LANG_TARGET,
-        tokenizer_path=config.TOKENIZER_FILE,
-    )
+    # # tokenize the dataset in both languages using the entire dataset
+    # logging.info("Tokenizing the dataset ...")
+    # src_tokenizer = TokenizeDataset(
+    #     dataset=lang_dataset.dataset["train"],
+    #     language=config.LANG_SRC,
+    #     tokenizer_path=config.TOKENIZER_FILE,
+    # )
+    # target_tokenizer = TokenizeDataset(
+    #     dataset=lang_dataset.dataset["train"],
+    #     language=config.LANG_TARGET,
+    #     tokenizer_path=config.TOKENIZER_FILE,
+    # )
 
-    # get the token ids for the dataset
-    logging.info("Getting the token ids ...")
-    src_data = src_tokenizer.get_token_ids()
-    target_data = target_tokenizer.get_token_ids()
+    # # get the token ids for the dataset
+    # logging.info("Getting the token ids ...")
+    # src_data = src_tokenizer.get_token_ids()
+    # target_data = target_tokenizer.get_token_ids()
 
-    # set the token ids for the dataset
-    lang_dataset.set_src_target_ids(src_data=src_data, target_data=target_data)
+    # # set the token ids for the dataset
+    # lang_dataset.set_src_target_ids(src_data=src_data, target_data=target_data)
 
-    # split the src and target ids into train, val and test sets
-    lang_dataset.split(
-        train_split=config.TRAIN_SPLIT,
-        val_split=config.VAL_SPLIT,
-        batch_size=config.BATCH_SIZE,
-    )
+    # # split the src and target ids into train, val and test sets
+    # lang_dataset.split(
+    #     train_split=config.TRAIN_SPLIT,
+    #     val_split=config.VAL_SPLIT,
+    #     batch_size=config.BATCH_SIZE,
+    # )
 
-    # get the loaders
-    (
-        src_train_loader,
-        src_val_loader,
-        src_test_loader,
-        target_train_loader,
-        target_val_loader,
-        target_test_loader,
-    ) = lang_dataset.get_loaders()
+    # # get the loaders
+    # (
+    #     src_train_loader,
+    #     src_val_loader,
+    #     src_test_loader,
+    #     target_train_loader,
+    #     target_val_loader,
+    #     target_test_loader,
+    # ) = lang_dataset.get_loaders()
 
-    for batch in src_test_loader:
-        print(batch)
+    # for batch in src_test_loader:
+    #     print(batch)
 
     # # logger.info("Splitting the dataset into train, val and test sets")
     # # # split the dataset
