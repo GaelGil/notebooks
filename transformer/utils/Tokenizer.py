f6f8a4e1721996133bfd3a5c79f908d5c73271e8
Gael Gil <gaelgilbello27@gmail.com>
2025-11-24 13:00:17 -0800

    preped for train

--- transformer/utils/Tokenizer.py
+++ transformer/utils/Tokenizer.py
@@ -14,21 +14,28 @@ class Tokenizer:
     def vocab_size(self):
         return self.sp.get_piece_size()
 
+    def write_txt(self, data: list, f: object):
+        # Write list to file
+        for line in data:
+            # strip whitespace just in case
+            line = line.strip()
+            if line:  # skip empty lines
+                f.write(line + "\n")
+
+    def create_joint_corpus(self, src_one, target_one, src_two, target_two):
+        # Combine into one file for tokenizer
+        with open(self.config.JOINT_CORPUS_PATH, "w", encoding="utf-8") as f:
+            for sentence_list in [src_one + src_two, target_one, target_two]:
+                self.write_txt(data=sentence_list, f=f)
+
     def train_tokenizer(self, src_one, target_one, src_two, target_two):
         os.makedirs(self.config.TOKENIZER_PATH, exist_ok=True)
 
-        # combine text into one file
-        temp_file = self.config.TOKENIZER_PATH + "_train.txt"
-        with open(temp_file, "w", encoding="utf-8") as f:
-            for s, t in zip(self.src_texts, self.tgt_texts):
-                f.write(s + "\n")
-                f.write(t + "\n")
+        self.create_joint_corpus(src_one, target_one, src_two, target_two)
 
-        # Train SentencePiece Unigram (T5 style)
-        self.sp = spm.SentencePieceTrainer.train(
-            input=temp_file,
+        spm.SentencePieceTrainer.Train(
+            input=self.config.JOINT_CORPUS_PATH,
             model_prefix=os.path.join(self.config.TOKENIZER_PATH, "joint"),
-            vocab_size=self.config["vocab_size"],
             model_type="unigram",
             character_coverage=1.0,
             pad_id=0,
@@ -36,15 +43,16 @@ class Tokenizer:
             bos_id=2,
             eos_id=3,
         )
-
+        # Load the trained tokenizer
+        self.sp.Load(self.config.TOKENIZER_MODEL_PATH)
         return self.sp
 
     def load_tokenizer(self):
-        self.sp.load(self.config.TOKENIZER_MODEL_PATH)
+        self.sp.Load(self.config.TOKENIZER_MODEL_PATH)
         return self.sp
 
     def encode(self, text: str, add_bos=True, add_eos=True):
-        ids = self.sp.encode(text, out_type=int)
+        ids = self.sp.Encode(text, out_type=int)
         if add_bos:
             ids = [self.sp.bos_id()] + ids
         if add_eos:
@@ -52,4 +60,4 @@ class Tokenizer:
         return ids
 
     def decode(self, ids):
-        return self.sp.decode(ids)
+        return self.sp.Decode(ids)
