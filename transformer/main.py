a8096e94186e72cd3d3aefbf62bf5f5c74d0a1db
Gael Gil <gaelgilbello27@gmail.com>
2025-11-24 13:00:05 -0800

    removed stuff

--- transformer/main.py
+++ transformer/main.py
@@ -39,9 +39,9 @@ def main():
         logging.info("Training the tokenizer ...")
         tokenizer = tokenizer.train_tokenizer(
             src_one=raw_src_one,
-            tgt_one=raw_target_one,
+            target_one=raw_target_one,
             src_two=raw_src_two,
-            tgt_two=raw_target_two,
+            target_two=raw_target_two,
         )
 
     # initialize the train state
@@ -67,66 +67,6 @@ def main():
 
     # state, step = checkpoint_manager.restore(state=state, logging=logging)
 
-    # load the dataset
-    # logging.info(f"Loading Dataset from: {config.DATA_PATH}")
-    # lang_dataset = LangDataset(
-    #     dataset_name=config.DATA_PATH,
-    #     src_lang=config.LANG_SRC,
-    #     target_lang=config.LANG_TARGET,
-    # )
-    # logging.info(f"Length of dataset: {lang_dataset.__len__()}")
-    # lang_dataset.handle_null()
-    # logging.info(f"Length of dataset after handling null: {lang_dataset.__len__()}")
-
-    # # tokenize the dataset in both languages using the entire dataset
-    # logging.info("Tokenizing the dataset ...")
-    # src_tokenizer = TokenizeDataset(
-    #     dataset=lang_dataset.dataset["train"],
-    #     language=config.LANG_SRC,
-    #     tokenizer_path=config.TOKENIZER_FILE,
-    # )
-    # target_tokenizer = TokenizeDataset(
-    #     dataset=lang_dataset.dataset["train"],
-    #     language=config.LANG_TARGET,
-    #     tokenizer_path=config.TOKENIZER_FILE,
-    # )
-
-    # # get the token ids for the dataset
-    # logging.info("Getting the token ids ...")
-    # src_data = src_tokenizer.get_token_ids()
-    # target_data = target_tokenizer.get_token_ids()
-
-    # # set the token ids for the dataset
-    # lang_dataset.set_src_target_ids(src_data=src_data, target_data=target_data)
-
-    # # split the src and target ids into train, val and test sets
-    # lang_dataset.split(
-    #     train_split=config.TRAIN_SPLIT,
-    #     val_split=config.VAL_SPLIT,
-    #     batch_size=config.BATCH_SIZE,
-    # )
-
-    # # get the loaders
-    # (
-    #     src_train_loader,
-    #     src_val_loader,
-    #     src_test_loader,
-    #     target_train_loader,
-    #     target_val_loader,
-    #     target_test_loader,
-    # ) = lang_dataset.get_loaders()
-
-    # for batch in src_test_loader:
-    #     print(batch)
-
-    # # logger.info("Splitting the dataset into train, val and test sets")
-    # # # split the dataset
-    # train_loader, val_loader, test_loader = dataset.split_data(
-    #     train_split=config.TRAIN_SPLIT,
-    #     val_split=config.VAL_SPLIT,
-    #     batch_size=config.BATCH_SIZE,
-    #     num_workers=config.NUM_WORKERS,
-    # )
     # # initialize the model
     # logger.info("Initializing the model and optimizer")
     # state = init_train_state(config)
