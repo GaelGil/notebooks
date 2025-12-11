f0cd0fdf0ce5aefb7f807f9a729adacae9f09265
Gael Gil <gaelgilbello27@gmail.com>
2025-12-10 17:23:28 -0800

    removed second dataset

--- transformer/main.py
+++ transformer/main.py
@@ -35,12 +35,12 @@ def main():
         target_lang=config.LANG_TARGET_ONE,
         seq_len=config.SEQ_LEN,
     )
-    dataset_two = LangDataset(
-        dataset_name=config.DATA_PATH,
-        src_lang=config.LANG_SRC_TWO,
-        target_lang=config.LANG_TARGET_TWO,
-        seq_len=config.SEQ_LEN,
-    )
+    # dataset_two = LangDataset(
+    #     dataset_name=config.DATA_PATH,
+    #     src_lang=config.LANG_SRC_TWO,
+    #     target_lang=config.LANG_TARGET_TWO,
+    #     seq_len=config.SEQ_LEN,
+    # )
 
     if config.SPLITS_PATH.exists():
         logging.info("Loading the splits ...")
@@ -51,18 +51,18 @@ def main():
                 target_name=config.LANG_TARGET_ONE,
             )
         )
-        (
-            src_two_train,
-            src_two_val,
-            _,
-            target_two_train,
-            target_two_val,
-            _,
-        ) = dataset_two.load_splits(
-            splits_path=config.SPLITS_PATH,
-            src_name=config.LANG_SRC_TWO,
-            target_name=config.LANG_TARGET_TWO,
-        )
+        # (
+        #     src_two_train,
+        #     src_two_val,
+        #     _,
+        #     target_two_train,
+        #     target_two_val,
+        #     _,
+        # ) = dataset_two.load_splits(
+        #     splits_path=config.SPLITS_PATH,
+        #     src_name=config.LANG_SRC_TWO,
+        #     target_name=config.LANG_TARGET_TWO,
+        # )
 
     if not Path(config.TOKENIZER_MODEL_PATH).exists():
         logging.info(
@@ -70,13 +70,11 @@ def main():
         )
         logging.info(f"Loading the data from {config.DATA_PATH} ...")
         raw_src_one, raw_target_one = dataset_one.load_data()
-        raw_src_two, raw_target_two = dataset_two.load_data()
+        # raw_src_two, raw_target_two = dataset_two.load_data()
         logging.info("Training a src and target tokenizer ...")
         tokenizer.train_tokenizer(
             src=raw_src_one,
             target=raw_target_one,
-            src_one=raw_src_two,
-            target_one=raw_target_two,
             prefixs=config.PREFIXES,
         )
 
@@ -86,7 +84,7 @@ def main():
         )
         logging.info(f"Loading the data from {config.DATA_PATH} ...")
         raw_src_one, raw_target_one = dataset_one.load_data()
-        raw_src_two, raw_target_two = dataset_two.load_data()
+        # raw_src_two, raw_target_two = dataset_two.load_data()
         tokenizer.load_tokenizer()
 
     if not config.SPLITS_PATH.exists():
@@ -96,11 +94,11 @@ def main():
             target=raw_target_one,
             prefix=config.PREFIXES[0],
         )
-        src_one, target_one = tokenizer.prep_data(
-            src=raw_src_two,
-            target=raw_target_two,
-            prefix=config.PREFIXES[1],
-        )
+        # src_one, target_one = tokenizer.prep_data(
+        #     src=raw_src_two,
+        #     target=raw_target_two,
+        #     prefix=config.PREFIXES[1],
+        # )
 
         logging.info("Splitting the data ...")
         src_one_train, src_one_val, _, target_one_train, target_one_val, _ = (
@@ -114,22 +112,22 @@ def main():
                 splits_path=config.SPLITS_PATH,
             )
         )
-        (
-            src_two_train,
-            src_two_val,
-            _,
-            target_two_train,
-            target_two_val,
-            _,
-        ) = dataset_two.split(
-            src=src_one,
-            target=target_one,
-            train_size=config.TRAIN_SPLIT,
-            val_size=config.VAL_SPLIT,
-            src_name=config.LANG_SRC_TWO,
-            target_name=config.LANG_TARGET_TWO,
-            splits_path=config.SPLITS_PATH,
-        )
+        # (
+        #     src_two_train,
+        #     src_two_val,
+        #     _,
+        #     target_two_train,
+        #     target_two_val,
+        #     _,
+        # ) = dataset_two.split(
+        #     src=src_one,
+        #     target=target_one,
+        #     train_size=config.TRAIN_SPLIT,
+        #     val_size=config.VAL_SPLIT,
+        #     src_name=config.LANG_SRC_TWO,
+        #     target_name=config.LANG_TARGET_TWO,
+        #     splits_path=config.SPLITS_PATH,
+        # )
 
     vocab_size = tokenizer.get_vocab_size()
 
@@ -202,31 +200,31 @@ def main():
             # target_tokenizer=tokenizer,
         )
 
-    train_loader = DataLoader(
-        src=src_two_train,
-        target=target_two_train,
-        batch_size=config.BATCH_SIZE,
-        seq_len=config.SEQ_LEN,
-        shuffle=True,
-    )
-    val_batch = DataLoader(
-        src=src_two_val,
-        target=target_two_val,
-        batch_size=config.BATCH_SIZE,
-        seq_len=config.SEQ_LEN,
-        shuffle=False,
-    )
+    # train_loader = DataLoader(
+    #     src=src_two_train,
+    #     target=target_two_train,
+    #     batch_size=config.BATCH_SIZE,
+    #     seq_len=config.SEQ_LEN,
+    #     shuffle=True,
+    # )
+    # val_batch = DataLoader(
+    #     src=src_two_val,
+    #     target=target_two_val,
+    #     batch_size=config.BATCH_SIZE,
+    #     seq_len=config.SEQ_LEN,
+    #     shuffle=False,
+    # )
 
-    logging.info("Training completed, training with new data")
-    train(
-        state=state,
-        train_loader=train_loader,
-        val_loader=val_batch,
-        epochs=config.EPOCHS,
-        manager=manager,
-        logger=logging,
-        scheduler=scheduler,
-    )
+    # logging.info("Training completed, training with new data")
+    # train(
+    #     state=state,
+    #     train_loader=train_loader,
+    #     val_loader=val_batch,
+    #     epochs=config.EPOCHS,
+    #     manager=manager,
+    #     logger=logging,
+    #     scheduler=scheduler,
+    # )
 
 
 if __name__ == "__main__":
