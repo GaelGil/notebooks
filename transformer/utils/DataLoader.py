a244061c560c32af476b928cc92358951e28b97e
Gael Gil <gaelgilbello27@gmail.com>
2025-12-22 09:42:49 -0800

    added comments

--- transformer/utils/DataLoader.py
+++ transformer/utils/DataLoader.py
@@ -3,55 +3,93 @@ from jax import numpy as jnp
 
 
 class Source(grain.DataLoader):
+    """
+    Source data loader
+    """
+
     def __init__(self, src_path: str, target_path: str, pad_id: int):
+        """
+        Args:
+            src_path: path to the source data
+            target_path: path to the target data
+            pad_id: pad id
+
+        Returns:
+            None
+        """
         self.src = jnp.load(src_path)
         self.target = jnp.load(target_path)
         self.pad_id = pad_id
 
     def __len__(self):
+        """
+        Returns:
+            length of the source data
+        """
         return len(self.src)
 
     def make_padding_mask(self, padded_ids):
-        return padded_ids != self.pad_id
+        """
+        Args:
+            padded_ids: padded ids
 
-    def make_src_mask(self, encoder_input_ids):
-        return self.make_padding_mask(encoder_input_ids)
+        Returns:
+            mask for padded ids (1, seq_len)
+        """
+        return padded_ids != self.pad_id
 
     def make_causal_mask(self, tgt_len: int):
         """
-        Returns a boolean mask shape (1, 1, tgt_len, tgt_len), True for allowed (i <= j).
-        Returns lower-triangular boolean mask shape (1, 1, tgt_len, tgt_len),
-        True for allowed (i >= j).
+        Args:
+            tgt_len: target length
+
+        Returns:
+            causal mask
         """
         tri = jnp.tril(jnp.ones((tgt_len, tgt_len), dtype=jnp.bool_))
-        return tri[None, None, :, :]  # (1,1,T,T)
+        return tri[None, None, :, :]
 
     def make_decoder_self_mask(self, decoder_input_ids):
+        """
+        Args:
+            decoder_input_ids: decoder input ids
+
+        Returns:
+            mask for decoder self-attention to ignore future tokens
+        """
         T = decoder_input_ids.shape[0]
         causal = jnp.tril(jnp.ones((T, T), dtype=jnp.bool_))
         key_nonpad = self.make_padding_mask(decoder_input_ids)
         return causal & key_nonpad[None, :]
 
     def make_encoder_decoder_mask(self, encoder_input_ids, decoder_input_ids):
+        """
+        Args:
+            encoder_input_ids: encoder input ids
+            decoder_input_ids: decoder input ids
+
+        Returns:
+            mask for encoder-decoder attention to ignore future tokens
+        """
         src_len = encoder_input_ids.shape[0]
         tgt_len = decoder_input_ids.shape[0]
 
-        enc_nonpad = self.make_padding_mask(encoder_input_ids)  # (S,)
+        enc_nonpad = self.make_padding_mask(encoder_input_ids)
 
-        # (1, T, S)
         return jnp.broadcast_to(
             enc_nonpad[None, None, :],
             (1, tgt_len, src_len),
         )
 
     def __getitem__(self, idx):
-        encoder_input = self.src[idx]  # encoder input ids padded
-        decoder_input = self.target[idx]  # decoder input ids padded
-        # print(encoder_input.shape)
+        encoder_input = self.src[idx]  # encoder input ids already padded
+        decoder_input = self.target[idx]  # decoder input ids already padded
 
         labels = decoder_input[1:]  # the labels are the decoder input shifted by one
-        labels_mask = self.make_padding_mask(labels)
-        decoder_input = decoder_input[:-1]
+        labels_mask = self.make_padding_mask(
+            labels
+        )  # mask for the labels to ignore padded tokens
+        decoder_input = decoder_input[:-1]  # remove the last token
 
         encoder_padding_mask = self.make_padding_mask(
             encoder_input
@@ -62,7 +100,7 @@ class Source(grain.DataLoader):
 
         encoder_decoder_mask = self.make_encoder_decoder_mask(
             encoder_input, decoder_input
-        )
+        )  # mask for encoder-decoder attention
 
         return (
             encoder_input,
