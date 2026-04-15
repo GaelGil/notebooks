ab1096b4ecd8bc9c15e7b545d1ca6d16d0b129b1
Gael Gil <gaelgilbello27@gmail.com>
2026-04-15 12:04:21 -0700

    cleaning up repo

--- transformer/append_metrics.py
+++ transformer/append_metrics.py
@@ -2,24 +2,24 @@
 import json
 import os
 
-txt_file = "/home/gg/git_repos/notebooks/transformer/chckpnts_phase2_mixed_model.txt"
-checkpoint_dir = "/home/gg/git_repos/notebooks/transformer/chckpnts_phase2_mixed_model"
+txt_file = "./metrics/chckpnts_phase2_mixed.txt"
+checkpoint_dir = "./chckpnts_phase2_mixed"
 
 # Get all checkpoint folders after 99
 epochs = []
 for name in os.listdir(checkpoint_dir):
     path = os.path.join(checkpoint_dir, name)
-    if os.path.isdir(path) and name.isdigit() and int(name) > 99:
+    if os.path.isdir(path) and name.isdigit() and int(name) > 0:
         epochs.append(int(name))
 
 epochs.sort()
 print(f"Found {len(epochs)} checkpoints: {epochs}")
 
 # Read metrics from each folder and append to file
-with open(txt_file, 'a') as f:
+with open(txt_file, "a") as f:
     for epoch in epochs:
         metrics_file = os.path.join(checkpoint_dir, str(epoch), "metrics", "metrics")
-        with open(metrics_file, 'r') as mf:
+        with open(metrics_file, "r") as mf:
             data = json.load(mf)
             train_loss = data["train_loss"]
             eval_loss = data["eval_loss"]
